36270c4ec9af66ce00d7d1b894a08f05b912cc3c
Russell Yanofsky <russ@yanofsky.org>
2008-03-16 17:09:00 +0000

    Create rails project directory with "rails fbdb"
    * trunk/fbdb
    
    
    git-svn-id: https://russ.yanofsky.org/svn/fbdb/trunk@2 b8161ba5-c0a9-442b-b824-12fe2e14d727

---
+++ fbdb/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '7646c5216bcdce7e692d718bde639104'
+end
