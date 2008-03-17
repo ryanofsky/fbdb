48fb143893001735e70fe7b7db1f62703ba79638
Russell Yanofsky <russ@yanofsky.org>
2008-03-17 12:04:23 +0000

    Get event and people lists to use descriptive text instead of object identities
    * fbdb/app/models/event.rb
    * fbdb/app/models/person.rb
    
    git-svn-id: https://russ.yanofsky.org/svn/fbdb/trunk@16 b8161ba5-c0a9-442b-b824-12fe2e14d727

--- fbdb/app/models/event.rb
+++ fbdb/app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
   set_table_name :EVENTS
   set_primary_key :EventID
   has_and_belongs_to_many :people, :join_table => :LINKPERSONSEVENTS, :foreign_key => :EventID, :association_foreign_key => :SSNO
+  def to_s
+    self.EventDate
+  end
 end
--- fbdb/app/models/person.rb
+++ fbdb/app/models/person.rb
@@ -3,4 +3,7 @@ class Person < ActiveRecord::Base
   set_primary_key :SSNO
   has_one :phone_address, :foreign_key => :SSNO
   has_and_belongs_to_many :events, :join_table => :LINKPERSONSEVENTS, :foreign_key => :SSNO, :association_foreign_key => :EventID
+  def to_s
+    "#{self.FIRSTNAME} #{self.LASTNAME}"
+  end
 end
